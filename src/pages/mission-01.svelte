50dbeaf2b3c89b3e93f60112ea5eb72333aec04e
coprolit <coprolit@users.noreply.github.com>
2020-10-26 08:42:44 +0000

    deploy: d51bab2c499d2c794bd21b8cd58605a572fca83b

--- src/pages/mission-01.svelte
+++
@@ -1,9 +0,0 @@
-<h1>Mission title 01</h1>
-
-<q><i>Some introductory intel-like fluff text...</i></q>
-
-<h2>OBJECTIVE</h2>
-<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut ultricies feugiat metus vitae sollicitudin. Donec commodo mollis lorem non tincidunt. Integer nec nisi ut enim tristique sollicitudin eget nec mi.</p>
-
-<h2>VICTORY</h2>
-<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut ultricies feugiat metus vitae sollicitudin.</p>
