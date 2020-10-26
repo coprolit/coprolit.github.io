e918263652e2d299ea5fefcf5e76998024a57cd3
Philippe Simpson <philippesimpson@gmail.com>
2020-10-26 18:29:46 +0100

    migrate to routify 2

---
+++ src/App.svelte
@@ -0,0 +1,10 @@
+<script>
+  import { Router } from "@roxi/routify";
+  import { routes } from "../.routify/routes";
+</script>
+
+<style  global>
+  @import "../static/global.css";
+</style>
+
+<Router {routes} />
---
+++ src/pages/mission-03.svelte
@@ -0,0 +1,9 @@
+<h1>Mission title 03</h1>
+
+<q><i>Some introductory intel-like fluff text...</i></q>
+
+<h2>OBJECTIVE</h2>
+<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut ultricies feugiat metus vitae sollicitudin. Donec commodo mollis lorem non tincidunt. Integer nec nisi ut enim tristique sollicitudin eget nec mi.</p>
+
+<h2>VICTORY</h2>
+<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut ultricies feugiat metus vitae sollicitudin.</p>
