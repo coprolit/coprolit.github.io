d0018a7cb6c5fc6f8f80fa1a380c9ca55b3f56ef
Philippe Simpson <philippesimpson@gmail.com>
2020-10-27 08:48:01 +0100

    test

--- src/pages/index.svelte
+++ src/pages/index.svelte
@@ -41,7 +41,6 @@
 
 <svelte:head>
     <script src="https://smtpjs.com/v3/smtp.js"></script>
-    <base href="https://coprolit.github.io/missions/">
 </svelte:head>
 
 <h1>Bolt Action side missions generator</h1>
