c06eea3874727f4172f215a1d058a4e3da2d4575
Philippe Simpson <philippesimpson@gmail.com>
2020-10-28 14:21:44 +0100

    Update and rename mission-01.svelte to Everybody-in.svelte

---
+++ src/pages/missions/Everybody-in.svelte
@@ -0,0 +1,10 @@
+Mission
+<h1>Everybody in</h1>
+
+<q><i>Some introductory intel-like fluff text...</i></q>
+
+<h2>OBJECTIVE</h2>
+<p>Play a game with no reserves used.</p>
+
+<h2>VICTORY</h2>
+<p>Score one point if you play a game with no reserves used.</p>
