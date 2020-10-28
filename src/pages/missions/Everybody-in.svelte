9f38f7e9a93afe718640b9e829d2021d77be91a0
Philippe Simpson <philippesimpson@gmail.com>
2020-10-28 16:28:24 +0100

    Update Everybody-in.svelte

--- src/pages/missions/Everybody-in.svelte
+++ src/pages/missions/Everybody-in.svelte
@@ -1,7 +1,8 @@
 Mission
 <h1>Everybody in</h1>
 
-<q><i>Some introductory intel-like fluff text...</i></q>
+<h2>INTEL</h2>
+<blockquote>Some introductory intel-like fluff text...</blockquote>
 
 <h2>OBJECTIVE</h2>
 <p>Play a game with no reserves used.</p>
