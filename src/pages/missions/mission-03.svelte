aed9aec956ead662d7b14c62cdf6a787101d306a
Philippe Simpson <philippesimpson@gmail.com>
2020-10-28 14:25:41 +0100

    Update mission-03.svelte

--- src/pages/missions/mission-03.svelte
+++ src/pages/missions/mission-03.svelte
@@ -1,9 +1,10 @@
-<h1>Mission title 03</h1>
+Mission:
+<h1>Reckless bravery</h1>
 
 <q><i>Some introductory intel-like fluff text...</i></q>
 
 <h2>OBJECTIVE</h2>
-<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut ultricies feugiat metus vitae sollicitudin. Donec commodo mollis lorem non tincidunt. Integer nec nisi ut enim tristique sollicitudin eget nec mi.</p>
+<p>Do not use a down order during the game.</p>
 
 <h2>VICTORY</h2>
-<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut ultricies feugiat metus vitae sollicitudin.</p>
+<p>Score one victory point if you do not use a down order during the game.</p>
