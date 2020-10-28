f6baae0bea9ea3790f63f0987331c47184d7bbc0
Philippe Simpson <philippesimpson@gmail.com>
2020-10-28 14:24:21 +0100

    Update and rename mission-02.svelte to Decapitation.svelte

---
+++ src/pages/missions/Decapitation.svelte
@@ -0,0 +1,10 @@
+Mission:
+<h1>Decapitation</h1>
+
+<q><i>Some introductory intel-like fluff text...</i></q>
+
+<h2>OBJECTIVE</h2>
+<p>Kill an enemy officer during the game.</p>
+
+<h2>VICTORY</h2>
+<p>Score one victory point if you kill an enemy officer during the game.</p>
