09a50fe6db0ee62652b19bb4aca89ac1f5cc4c65
Philippe Simpson <philippesimpson@gmail.com>
2020-10-28 15:53:25 +0100

    Create The-Mole.svelte

---
+++ src/pages/missions/The-Mole.svelte
@@ -0,0 +1,21 @@
+Missions:
+<h1>The Mole</h1>
+
+<q><i>INTEL: HQ has made contact with a friendly local close to enemy lines. The local has important information on the enemy positions. Aid in his rescue before the vital information he has falls into enemy hands!</i></q>
+
+<h2>SETUP</h2>
+<p>
+  Just before beginning the first turn of the game, after all deployment is finished (including snipers, spotters and observers), you place a model representing the lone local on the table, following the same set-up instructions for the objective in the Hold Until Relieved scenario on page 114 of the Bolt Action rulebook.
+  Special rules: The local has no weapons and cannot Run in the first turn of the game.
+</p>
+
+<h2>OBJECTIVE</h2>
+<p>
+  The local needs to leave the table by moving into contact with your own table edge.
+</p>
+
+<h2>VICTORY</h2>
+<p>
+  If the local manages to leave the table, when the game ends you treat a defeat result in the scenario being played as a draw, and a draw result as a victory.
+  On the other hand, if the local is killed before he leaves the table, when the game ends the opponent treats a defeat result in the scenario being played as a draw, and a draw result as a victory.
+</p>
