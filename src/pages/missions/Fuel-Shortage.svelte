784a656652748492c1074df94187adf5410722d0
Philippe Simpson <philippesimpson@gmail.com>
2020-10-28 21:58:10 +0100

    Create Fuel-Shortage.svelte

---
+++ src/pages/missions/Fuel-Shortage.svelte
@@ -0,0 +1,15 @@
+Mission:
+<h1>Fuel Shortage</h1>
+
+<h2>INTEL</h2>
+<blockquote>Our supply lines are failing. We must conserve fuel!</blockquote>
+
+<h2>OBJECTIVE</h2>
+<p>
+  A vehicle model must not be given an Advance or Run order for 3 consecutive turns.
+</p>
+
+<h2>VICTORY</h2>
+<p>
+  At the end of the game, score one victory point if you achieved your objective.
+</p>
