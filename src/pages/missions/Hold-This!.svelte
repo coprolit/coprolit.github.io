537aa02829535fbaaf5923f23b4b723d68eda3c7
Philippe Simpson <philippesimpson@gmail.com>
2020-10-29 13:25:28 +0100

    Create Hold-This!.svelte

---
+++ src/pages/missions/Hold-This!.svelte
@@ -0,0 +1,21 @@
+Mission:
+<h1>Hold This!</h1>
+
+<h2>INTEL</h2>
+<blockquote>We have seized a key strategic position. Hold it until relieved.</blockquote>
+
+<h2>SETUP</h2>
+<p>
+  During setup, before deployment, you place an objective marker in your deployment zone (or table edge).
+  Just before beginning the first turn of the game, after all deployment is finished (including snipers, spotters and observers), designate a non-vehicle unit to defend the objective.
+</p>
+
+<h2>OBJECTIVE</h2>
+<p>
+  From turn 2 and for the rest of the game, the designated unit must end its turn within 3” of the objective marker.
+</p>
+
+<h2>VICTORY</h2>
+<p>
+  If you achieve your mission, score +1 point.
+</p>
