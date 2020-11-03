592113328d50d485170123b1d0e7dd0a785da4ec
Dennis Flood <flood@tekstur.dk>
2020-11-03 21:59:12 +0100

    Create Probe mission

---
+++ src/pages/missions/Probe-Defenses.svelte
@@ -0,0 +1,11 @@
+Mission
+<h1>Probe enemy defences</h1>
+
+<h2>INTEL</h2>
+<blockquote>We need a handle on the enemy positions</blockquote>
+
+<h2>OBJECTIVE</h2>
+<p>The first unit to be pinned must fall back to your own lines to report enemy positions.</p>
+
+<h2>VICTORY</h2>
+<p>Score one point if first unit to be pinned executes consecutive runs back to own table edge.</p>
