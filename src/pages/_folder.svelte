00c3f7e26ffeccd5c2510f3adcdaa17a39bcaf63
Philippe Simpson <philippesimpson@gmail.com>
2020-11-04 15:02:31 +0100

    fancier logo

--- src/pages/_folder.svelte
+++ src/pages/_folder.svelte
@@ -6,7 +6,10 @@
 
 <header>
   <div class="logo">
-    <a href={$url()}>Bolt Action mission generator</a>
+    <a href={$url()}>
+      <h1>Mission generator</h1>
+    </a>
+    <img src="https://cdn.shopify.com/s/files/1/0255/0949/4864/files/NEW_Bolt_Action_logo_BA_header_480x480.png">
   </div>
   <nav>
     {#each missions as node}
