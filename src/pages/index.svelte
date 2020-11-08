fc6b673aafd21f3de086e5bb3954995931e8ce88
Philippe Simpson <philippesimpson@gmail.com>
2020-11-08 18:03:39 +0100

    add rulebook scenarios

--- src/pages/index.svelte
+++ src/pages/index.svelte
@@ -5,41 +5,100 @@
 
     const emailPattern = "[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,}$";
 
-    function getRandomInt(max) {
-        return Math.floor(Math.random() * Math.floor(max));
+    let selected;
+    let includeMissions = false;
+    let includeBattleScenarios = false;
+    let includeAttDefScenarios = false;
+    let scenario = false;
+
+    let send1 = false;
+    let send2 = false;
+
+    function getRandomInt(max, min = 0) {
+        return Math.floor(Math.random() * (max - min + 1) + min);
     }
 
     function generate(event) {
-        const urls = $layout.parent.children
+        // Reset:
+        send1, send2, scenario = false;
+
+        if (includeMissions) {
+            assignMissions(event.target.email1.value, event.target.email2.value);
+        }
+        
+        if (includeBattleScenarios || includeAttDefScenarios) {
+            scenario = getScenario(includeBattleScenarios, includeAttDefScenarios);
+        }
+        
+    }
+
+    function assignMissions(email1, email2) {
+        
+        const missions = $layout.parent.children
             .filter(child => child.path === '/missions')[0]
             .children
-            .map(child => child.path.slice(1));
+            .map(child => {
+                return {
+                    path: window.location.href + child.path.slice(1),
+                    title: child.title
+                }
+            });
 
         // Assign mission to player #1:
-        const randomUrl1 = window.location.href + urls[getRandomInt(urls.length)];
-
+        const randomMission1 = missions[getRandomInt(missions.length - 1)];
+        
         Email.send({
-            SecureToken : "0b6eed09-ca2b-41ed-9283-2d33686b5dd2",
-            To : event.target.email1.value,
-            From : "missions@philippesimpson.dk",
-            Subject : "You have been assigned a secret side mission",
-            Body : `See your mission <a href=${randomUrl1}>here</a>`
+            SecureToken: "0b6eed09-ca2b-41ed-9283-2d33686b5dd2",
+            To: email1,
+            From: "missions@philippesimpson.dk",
+            Subject: "You have been assigned the mission: " + randomMission1.title,
+            Body: `See your mission <a href=${randomMission1.path}>here</a>`
         }).then(
-            message => console.log(message, event.target.email1.value, randomUrl1)
+            message => send1 = true
         );
         
         // Assign mission to player #1:
-        const randomUrl2 = window.location.href + urls[getRandomInt(urls.length)];
+        const randomMission2 = missions[getRandomInt(missions.length - 1)];
 
         Email.send({
-            SecureToken : "0b6eed09-ca2b-41ed-9283-2d33686b5dd2",
-            To : event.target.email2.value,
-            From : "missions@philippesimpson.dk",
-            Subject : "You have been assigned a secret side mission",
-            Body : `See your mission <a href=${randomUrl2}>here</a>`
+            SecureToken: "0b6eed09-ca2b-41ed-9283-2d33686b5dd2",
+            To: email2,
+            From: "missions@philippesimpson.dk",
+            Subject: "You have been assigned the mission: " + randomMission2.title,
+            Body: `See your mission <a href=${randomMission2.path}>here</a>`
         }).then(
-            message => console.log(message, event.target.email2.value, randomUrl2)
+            message => send2 = true
         );
+    }
+
+    function getScenario(battle, attackerDefender) {
+
+        const scenarios = [
+            "Scenario 1 – No Man's Land (Rulebook p.134)",
+            "Scenario 2 – Meeting Engagement (Rulebook p.135)",
+            "Scenario 3 – Key Positions (Rulebook p.136)",
+            "Scenario 4 – Double Envelopment (Rulebook p.137)",
+            "Scenario 5 – Top Secret (Rulebook p.138)",
+            "Scenario 6 – Demolition (Rulebook p.139)",
+            "Scenario 7 – Envelopment (Rulebook p.140)",
+            "Scenario 8 – Manhunt (Rulebook p.142)",
+            "Scenario 9 – Point Defence (Rulebook p.144)",
+            "Scenario 10 – Hold Until Relieved (Rulebook p.145)",
+            "Scenario 11 – Surrounded! (Rulebook p.146)",
+            "Scenario 12 – Sectors (Rulebook p.148)"
+        ]
+
+        if (battle && attackerDefender) {
+            return scenarios[getRandomInt(11)];
+        }
+
+        if (battle) {
+            return scenarios[getRandomInt(5)];
+        }
+
+        if (attackerDefender) {
+            return scenarios[getRandomInt(11, 6)];
+        }
     }
 </script>
 
@@ -59,28 +118,84 @@
 
 <img src="/images/lpkb2gkaw4s51.jpg" alt="mission briefing" />
 
+<h1>Assign objectives</h1>
+
 <p>
-    Randomly assign a secret side mission to each player:
+    Randomly assign scenario and/or secret missions:
 </p>
 
-<form on:submit|preventDefault={generate}>
-    <input
-        type="email"
-        name="email1"
-        pattern={emailPattern}
-        title="player 1 email address"
-        required
-        placeholder="Player's email">
-    vs.
-    <input
-        type="email"
-        name="email2"
-        pattern={emailPattern}
-        title="player 2 email address"
-        required
-        placeholder="Player's email">
-    
-    <input
-        type="submit"
-        value="Submit">
-</form>
+<div class="shadow padding flow border">
+    <form 
+        on:submit|preventDefault={generate}
+        class="flow"
+    >
+        <div>
+            <div>
+                <input
+                    type="checkbox"
+                    id="battle"
+                    bind:checked={includeBattleScenarios}
+                >
+                <label for="battle">Rulebook Battle scenarios</label>
+            </div>
+            <div>
+                <input
+                    type="checkbox"
+                    id="attacker-defender"
+                    bind:checked={includeAttDefScenarios}
+                >
+                <label for="attacker-defender">Rulebook Attacker-Defender scenarios</label>
+            </div>
+        </div>
+        <div>
+            <input
+                type="checkbox"
+                id="mission"
+                bind:checked={includeMissions}>
+            <label for="mission">Secret missions</label>
+
+            {#if includeMissions}
+            <div>
+                <input
+                    type="email"
+                    name="email1"
+                    pattern={emailPattern}
+                    title="player 1 email address"
+                    required
+                    placeholder="Player's email">   
+                vs.
+                <input
+                    type="email"
+                    name="email2"
+                    pattern={emailPattern}
+                    title="player 2 email address"
+                    required
+                    placeholder="Player's email"
+                >
+            </div>
+            {/if}
+        </div>
+        
+        <div>
+            <input
+                type="submit"
+                value="Generate">
+        </div>
+    </form>
+
+    {#if scenario}
+    <div class="border-top"></div>
+    <div>
+        You are to face your opponent in:
+    </div>
+    <blockquote>{scenario}</blockquote>
+    {/if}
+    {#if (send1 && send2)}
+    <div class="border-top"></div>
+    <div>
+        Secret missions have been assigned and send to each player. <span class="check-mark">&check;</span>
+    </div>
+    {/if}
+</div>
+
+
