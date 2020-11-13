b0901c3937b97d0e4f9acf668a972a4b713a8506
Philippe Simpson <philippesimpson@gmail.com>
2020-11-13 12:52:23 +0100

    text update

--- src/pages/missions/Reckless-bravery.svelte
+++ src/pages/missions/Reckless-bravery.svelte
@@ -1,7 +1,7 @@
 <h1>Reckless bravery</h1>
 
 <h2>Intel</h2>
-<blockquote>Charge! We must charge at all cost!</blockquote>
+<blockquote>We must act in a strong, determined way to defeat the enemy. We must press the attack at all cost!</blockquote>
 
 <h2>Objective</h2>
 <p>Do not use a Down order during a turn (at turn end phase, none of your units have a Down order next to them).</p>
