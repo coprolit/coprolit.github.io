e2895d67352ac824e883002415c8c9b81ad7171f
Philippe Simpson <philippesimpson@gmail.com>
2020-11-13 20:45:30 +0100

    fix The Mole mission

--- src/pages/missions/The-Mole.svelte
+++ src/pages/missions/The-Mole.svelte
@@ -1,15 +1,15 @@
 <h1>The Mole</h1>
 
 <h2>Intel</h2>
-<blockquote>HQ has made contact with a friendly local close to enemy lines. The local has important information on the enemy positions. Aid in his rescue before the vital information he has falls into enemy hands!</blockquote>
+<blockquote>A key member of the resistance is hiding close to enemy lines. Aid in his rescue before the vital information he has falls into enemy hands!</blockquote>
 
 <h2>Setup</h2>
 <p>
-  Just before beginning the first turn of the game, after all deployment is finished (including snipers, spotters and observers), you place a model representing the lone local on the table, following the same set-up instructions for the objective in the Hold Until Relieved scenario on page 114 of the Bolt Action rulebook.
-  Special rules: The local has no weapons and cannot Run in the first turn of the game.
+  Just before beginning the first turn of the game, after all deployment is finished (including snipers, spotters and observers), you place an objective marker representing the hiding resistance operative on the table, following the same set-up instructions for the objective in the Hold Until Relieved scenario on page 114 of the Bolt Action rulebook.
 </p>
 
 <h2>Objective</h2>
 <p>
-  The local needs to leave the table by moving into contact with your own table edge.
+  You must move to within 6” of the resistance operative with a infantry unit at which point the resistance operative will reveal themselves (place another model on the objective, at which point they join the friendly unit as a regular, pistol-armed soldier) if there is no enemy unit within 6”.
+  The operative needs to leave the table by moving into contact with your own table edge.
 </p>
