4f2993725aabf09e41883b89f93ccb3511dc1498
Philippe Simpson <philippesimpson@gmail.com>
2020-11-14 19:16:57 +0100

    Update Envelopment.svelte
    - adjust objective for missions with no outflank.

--- src/pages/missions/Envelopment.svelte
+++ src/pages/missions/Envelopment.svelte
@@ -1,9 +1,9 @@
 <h1>Envelopment</h1>
 
 <h2>Situation</h2>
-<blockquote>We need to threaten the enemy's communications and line of retreat. Flank and make a push to the enemy's rear.</blockquote>
+<blockquote>We need to threaten the enemy's communications and line of retreat. Make a push to the enemy's rear.</blockquote>
 
 <h2>Objective</h2>
 <p>
-  During the game have a unit move onto the table from outflank and end its turn in the enemy's table half.
+  At the end of the game you must have a unit within 6” of opponent's table edge.
 </p>
