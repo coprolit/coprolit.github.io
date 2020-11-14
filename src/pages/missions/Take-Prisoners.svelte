826cea0c9b93a62294db2a5c9af637a6d4a284d2
Philippe Simpson <philippesimpson@gmail.com>
2020-11-14 20:37:42 +0100

    Delete Take-Prisoners.svelte

--- src/pages/missions/Take-Prisoners.svelte
+++
@@ -1,7 +0,0 @@
-<h1>Take Prisoners</h1>
-
-<h2>Intel</h2>
-<blockquote>You are tasked with taking a prisoner for intel.</blockquote>
-
-<h2>Objective</h2>
-<p>Defeat any enemy infantry or artilery unit in close combat and you will have a prisoner. Move this prisoner behind the frontline before the end of the game. Keep the prisoner alive and under control.</p>
