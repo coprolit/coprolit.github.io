946e31e0bd13ad51469a2fed657076f01e0fed79
Philippe Simpson <philippesimpson@gmail.com>
2020-11-19 12:05:11 +0100

    Update and rename Everybody-in.svelte to Shock-and-awe.svelte

--- src/pages/missions/Everybody-in.svelte
+++
@@ -1,7 +0,0 @@
-<h1>Everybody in</h1>
-
-<h2>Intel</h2>
-<blockquote>We must use overwhelming power to try and achieve rapid dominance over the enemy.</blockquote>
-
-<h2>Objective</h2>
-<p>At the end of turn 3 you must have 3 or more units fully or partially within the opponent's table half.</p>
---
+++ src/pages/missions/Shock-and-awe.svelte
@@ -0,0 +1,7 @@
+<h1>Shock and awe</h1>
+
+<h2>Intel</h2>
+<blockquote>We must use overwhelming power and spectacular display of force to try and achieve rapid dominance over the enemy and destroy their will to fight.</blockquote>
+
+<h2>Objective</h2>
+<p>At the end of turn 3 you must have 3 or more units fully or partially within the opponent's table half.</p>
