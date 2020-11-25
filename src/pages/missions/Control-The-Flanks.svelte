6a77631bf71dd98c08f2c70907e7b55cd7dd1704
Philippe Simpson <philippesimpson@gmail.com>
2020-11-25 21:53:11 +0100

    add mission Control the Flanks

---
+++ src/pages/missions/Control-The-Flanks.svelte
@@ -0,0 +1,7 @@
+<h1>Control The Flanks</h1>
+
+<h2>Intel</h2>
+<blockquote>We need to control the flanks and force the enemy to fight from multiple directions.</blockquote>
+
+<h2>Objective</h2>
+<p>At the end of the game, you must have one or more units within 6” of each table side edge on the opponent's half of the table.</p>
