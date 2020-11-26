7498c5edc09e65f8a421830d39e48815020b879a
Philippe Simpson <philippesimpson@gmail.com>
2020-11-26 13:24:46 +0100

    update Control the Flanks

--- src/pages/missions/Control-The-Flanks.svelte
+++ src/pages/missions/Control-The-Flanks.svelte
@@ -4,4 +4,4 @@
 <blockquote>We need to control the flanks and force the enemy to fight from multiple directions.</blockquote>
 
 <h2>Objective</h2>
-<p>At a turn end phase, have one or more units within 9” of each table side edge on the opponent's half of the table.</p>
+<p>At a turn end phase, have one or more units partially or fully within 9” of each table side edge on the opponent's half of the table.</p>
