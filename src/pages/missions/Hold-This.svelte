e8957195ac37b0e3517b16bbbd528f9140c870d6
Philippe Simpson <philippesimpson@gmail.com>
2020-11-28 15:52:22 +0100

    add image to mission Shock and Awe

--- src/pages/missions/Hold-This.svelte
+++ src/pages/missions/Hold-This.svelte
@@ -8,11 +8,11 @@
   During setup, before deployment, you place an objective marker (roughly 2” in diameter) within 12” of your table edge.
 </p>
 
-<h2>Objective</h2>
-<p>
-  At the end of every turn of the game a model of a friendly unit must be within 3” of the objective marker.
-</p>
-
 <figure>
   <img src="https://docs.google.com/drawings/d/e/2PACX-1vRjI8MfSEEU7N2OdjGw8W2ldgyEhEfEvXaJt-7mpzX0ZiwqM7XWiEyONNyAQG2uNwlPJb7qS-4Hxy_w/pub?w=1728&amp;h=1153">
 </figure>
+
+<h2>Objective</h2>
+<p>
+  At the <strong>end of every turn</strong> of the game a model of a friendly unit must be within 3” of the objective marker.
+</p>
--- src/pages/missions/Shock-and-awe.svelte
+++ src/pages/missions/Shock-and-awe.svelte
@@ -1,7 +1,11 @@
 <h1>Shock and awe</h1>
 
-<h2>Intel</h2>
+<h2>Situation</h2>
 <blockquote>We must use overwhelming power and spectacular display of force to try and achieve rapid dominance over the enemy and destroy their will to fight.</blockquote>
 
 <h2>Objective</h2>
-<p>At the end of turn 3 you must have 3 or more units fully or partially within the opponent's table half.</p>
+<p>At the <strong>end of turn 3</strong> you must have <strong>3 or more units</strong> fully or partially within the opponent's table half.</p>
+
+<figure>
+  <img src="https://docs.google.com/drawings/d/e/2PACX-1vSi--Q68xwWOFCMTW0rcUkH0UjOSbuTlWxaJrbc28tG5HNaAMFvlESpvkNqqu6cyHoWkjUn0M6kIEtp/pub?w=1728&amp;h=1153">
+</figure>
