9dac433f1c2afcdc1a10660793924e2da79a6845
Philippe Simpson <philippesimpson@gmail.com>
2020-11-28 15:38:06 +0100

    add image to mission Hold This!

--- src/pages/missions/Hold-This.svelte
+++ src/pages/missions/Hold-This.svelte
@@ -1,14 +1,18 @@
 <h1>Hold This!</h1>
 
-<h2>Intel</h2>
+<h2>Situation</h2>
 <blockquote>We have seized a key strategic position. Hold it until relieved.</blockquote>
 
 <h2>Setup</h2>
 <p>
-  During setup, before deployment, you place an objective marker (roughly 2” in diameter) in your deployment zone (or within 6” of your table edge).
+  During setup, before deployment, you place an objective marker (roughly 2” in diameter) within 12” of your table edge.
 </p>
 
 <h2>Objective</h2>
 <p>
   At the end of every turn of the game a model of a friendly unit must be within 3” of the objective marker.
 </p>
+
+<figure>
+  <img src="https://docs.google.com/drawings/d/e/2PACX-1vRjI8MfSEEU7N2OdjGw8W2ldgyEhEfEvXaJt-7mpzX0ZiwqM7XWiEyONNyAQG2uNwlPJb7qS-4Hxy_w/pub?w=1728&amp;h=1153">
+</figure>
